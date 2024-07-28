30ec6db1a48b86872015a8670fce78065e696e05
zavaladrian <zavaladrian@gmail.com>
2024-07-28 12:50:37 -0500

    changed hero size for wide screen

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 const Hero = () => {
   return (
-    <section className="text-slat-100 overflow-hidden bg-gradient-to-t from-glacier to-antwhite px-8 py-24 tablet:px-12 tablet:py-40 laptop:py-60 h-screen w-auto dark:bg-slate-800  dark:bg-none  ">
+    <section className="text-slat-100 overflow-hidden bg-gradient-to-t from-glacier to-antwhite px-8 py-24 tablet:px-12 tablet:py-40 laptop:py-60 h-auto w-auto dark:bg-slate-800  dark:bg-none  ">
       <div className="relative mx-auto max-w-5xl">   
         <div className='float-right'>
 
