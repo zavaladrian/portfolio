ee6150b61602fc07e93d8c61f55ad58d10cb7f75
zavaladrian <zavaladrian@gmail.com>
2024-07-30 13:49:33 -0500

    fixed loading sc

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,8 @@
-'use client'
- import ShuffleLoader from "./projects/components/ShuffleLoader"
 
 export default function Loading() {
     return (
-      <div className="grid h-72 place-content-center bg-neutral-950 p-4">
-      <ShuffleLoader />
+      <div>
+        <p className="text-6xl text-white bg-black h-screen"> Loading...</p>
     </div>
 )
   }
