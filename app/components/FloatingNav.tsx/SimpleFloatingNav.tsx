b02ce6baa784f3fde6055c833f7f4bed17acd210
zavaladrian <zavaladrian@gmail.com>
2024-09-10 18:28:04 -0500

    changes

--- app/components/FloatingNav.tsx/SimpleFloatingNav.tsx
+++ app/components/FloatingNav.tsx/SimpleFloatingNav.tsx
@@ -1,49 +1,42 @@
-'use client'
+"use client";
 import React from "react";
 import Link from "next/link";
-import ToggleWrapper from "../toggle/DarkModeToggle";
-
 
 const LINKS = [
-    {
-      title: "Top",
-      href: "/#",
-    },
-    {
-      title: "About",
-      href: "/#about",
-    },
-    {
-      title: "Projects",
-      href: "/#projects",
-    },
-  
-  ];
-  
-
+  {
+    title: "Top",
+    href: "/#",
+  },
+  {
+    title: "About",
+    href: "/#about",
+  },
+  {
+    title: "Projects",
+    href: "/#projects",
+  },
+];
 
 export const SimpleFloatingNav = () => {
   return (
     <nav className="fixed left-[50%] top-8  w-fit -translate-x-[50%] items-center gap-6 rounded-2xl border-[1px] border-neutral-700 bg-glacier dark:bg-lavender p-2 text-lg text-neutral-900 z-30 hidden laptop:flex font-playwrite font-bold ">
-     {LINKS.map((l, idx) => {
-        return(
-            <Link href={l.href} key={l.title} >
-                {l.title}
-            </Link>
-        )
-     })}   
+      {LINKS.map((l, idx) => {
+        return (
+          <Link href={l.href} key={l.title}>
+            {l.title}
+          </Link>
+        );
+      })}
       <ContactButton />
-      {/* <ToggleWrapper /> */}
     </nav>
   );
 };
 
-
 const ContactButton = () => {
   return (
     <Link href="/#contact">
-    <button
-      className={`
+      <button
+        className={`
           relative z-0 flex items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg border-[1px] 
           border-neutral-700 px-4 py-1.5 font-medium
          text-neutral-900 transition-all duration-300
@@ -59,9 +52,9 @@ const ContactButton = () => {
           hover:scale-105 hover:border-neutral-50 hover:text-neutral-900
           hover:before:translate-y-[0%]
           active:scale-100`}
-    >
-      Contact
-    </button>
+      >
+        Contact
+      </button>
     </Link>
   );
 };
